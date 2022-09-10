0b292406ad1c6549660cdc15597a6a47153dba65
ozgesert <ozgesert@yahoo.com>
2022-09-10 21:58:20 +0300

    10.09 dersin ikinci yarısında koleksiyonlarda kaldım.

---
+++ OOP1/ProductManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP1
+{
+    class ProductManager
+    {
+        // void : emir kipi gibi bir şey
+        public void Add(Product product, Product product2)
+        {
+            Console.WriteLine("eklendi : " +product.ProductName+   " eklendi :"+  product2.ProductName);
+
+        }
+
+        public void Update(Product product, Product product2)
+        {
+            Console.WriteLine("eklendi : " + product.ProductName + " eklendi :" + product2.ProductName);
+
+        }
+
+        public int Topla(int sayi1, int sayi2)
+        {
+            return sayi1 + sayi2;
+        }
+
+        public void Topla2(int sayi1, int sayi2)
+        {
+            Console.WriteLine(sayi1 + sayi2);
+           
+        }
+    }
+}
